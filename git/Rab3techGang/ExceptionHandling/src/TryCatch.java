0b4a6bbf58f5cfaf1457153f4f5c556d5bebd8c5
kushal <kushalshrestha0808@gmail.com>
2023-02-16 12:47:58 -0500

    first commit

---
+++ git/Rab3techGang/ExceptionHandling/src/TryCatch.java
@@ -0,0 +1,21 @@
+
+public class TryCatch {
+
+	public static void main(String[] args) {
+		System.out.println("----------lets start the code------------");
+	int a=10;
+	int b=0;
+	int z;
+	try {
+		z=a/b;
+		System.out.println(z);
+	}
+	catch(Exception e){
+		System.out.println(e);
+	}
+
+System.out.println("---------lets end the code-----------");
+
+	}
+
+}
